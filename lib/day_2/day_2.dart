eef6e3f2e4705104c4b4232978568929619069b5
yakupbilgen <yakupbilgen92@gmail.com>
2021-12-22 23:48:13 +0300

    add image

---
+++ lib/day_2/day_2.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Day2 extends StatelessWidget {
+  const Day2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
